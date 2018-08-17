6a7afdab231429061f0237d9e0d83bddfec4f708
ABhilash chaparala <42341209+abi8186@users.noreply.github.com>
2018-08-17 16:00:19 +0530

    fgsf

---
+++ First programe/Untitled1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+	int age;
+	float weight;
+	printf ("enter your age and weight\n");
+	scanf("%d%f",&age,&weight);
+	printf("your age is %d\n your weight is %f",age,weight);
+	
+
+getch ();
+return 0;
+}
---
+++ First programe/unititled 2.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int counter;
+	for( counter = 1; counter <= 20; counter++){
+	if(counter == 5){
+	
+    break;
+}
+	printf("%d\n",counter); 
+
+	}
+	
+	
+	printf("what the f\n");
+
+getch();
+return 0;
+}
